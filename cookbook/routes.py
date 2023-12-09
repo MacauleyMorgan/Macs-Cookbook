b91a841c7b83213296882dfd877ec1e85a6bcc8e
Macauley Morgan <macauley.main@gmail.com>
2023-12-09 17:06:25 +0000

    Linked submit forms Forms send request to the back end

--- cookbook/routes.py
+++ cookbook/routes.py
@@ -2,8 +2,13 @@ from flask import render_template, render_template
 from cookbook import app, db
 from cookbook.models import User, Recipe 
 
-@app.route("/")
+@app.route("/",   methods =['GET', 'POST'])
 def landing():
+    if request.method == 'POST':
+        email = request.form.get("sign-in-email-address")
+        password = request.form.get("password")
+        user = [email, password]
+        print(user)
     return render_template("landing.html")
 
 
@@ -22,8 +27,15 @@ def admin():
     return render_template("admin.html")
 
 
-@app.route("/register")
+@app.route("/register", methods =['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        first_name = request.form.get("first-name")
+        last_name = request.form.get("last-name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = [first_name, last_name, email, password]
+        print(user)
     return render_template("register.html")
 
 
