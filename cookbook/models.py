21f6936b61f2d2304fdfdd23745380a2e1f50e85
Macauley Morgan <macauley.main@gmail.com>
2023-12-09 16:10:36 +0000

    Error in file, created db models to import db

--- cookbook/models.py
+++ cookbook/models.py
@@ -1,7 +1,23 @@
-from flask import render_template
-from cookbook import app, db
-from cookbook.models import User, Recipe 
+from cookbook import db
 
-@app.route("/")
-def home():
-    return render_template("base.html")
+class User(db.model):
+    # Schema for creating user in db
+    id = db.Column(db.integer, primary_key=True)
+    first_name = db.column(string(25), unique=False, nullable=False)
+    recipes = db.relationship("recipe", backref="user", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        return "#{0} - First Name: {1} Last name:{2}".format(
+            self.id, self.first_name, self.is_urgent
+        )
+
+
+class Recipe(db.model):
+    # Schema for creating user in db
+    id = db.Column(db.integer, db.foreign_key("User.id"), ondelete="CASCADE")
+    recipe_name = db.Column(db.text, nullable=False)
+    recipe_time = db.Column(db.integer(10), nullable=False)
+    recipe_ingredients = db.Column(db.text, nullable=False)
+
+    def __repr__(self):
+    return self.recipe_name
