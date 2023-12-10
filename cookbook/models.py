7987123f1cdd5f067111fbe5374128e1b48d6c16
Macauley Morgan <macauley.main@gmail.com>
2023-12-10 08:50:48 +0000

    Changed db reipe class repr function returns recipe info now

--- cookbook/models.py
+++ cookbook/models.py
@@ -11,8 +11,8 @@ class User(db.Model):
     recipes = db.relationship("recipe", backref="user", cascade="all, delete", lazy=True)
 
     def __repr__(self):
-        return "#{0} - First Name: {1} Last name:{2}".format(
-            self.id, self.first_name, self.is_urgent
+        return "#{0} - First Name: {1} Last name:{2} Email: {3} Password: {4} Is admin: {5}".format(
+            self.id, self.first_name, self.last_name, self.email, self.password, self.is_admin
         )
 
 
@@ -26,4 +26,6 @@ class Recipe(db.Model):
     recipe_steps = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
-        return self.recipe_name
+        return "Recipe Name {0} Recipe Time {1} Ingredients {2} Steps {3}".format(
+            self.recipe_name, self.recipe_time, self.recipe_ingredients, self.recipe_steps
+        )
