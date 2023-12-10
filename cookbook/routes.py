57ca19c01484228ce23f45ea7bed7a3645f07baa
Macauley Morgan <macauley.main@gmail.com>
2023-12-10 09:07:27 +0000

    Reorganised routes.py Added comments to map out register function

--- cookbook/routes.py
+++ cookbook/routes.py
@@ -11,6 +11,23 @@ def landing():
         user = [email, password]
         print(user)
     return render_template("landing.html")
+    
+
+@app.route("/register", methods =['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        """
+        This function will take the username and check if it exists on the database
+        If not, it will create a user
+        """
+        # Step 1 Check if username exists
+        # Step 2 If user exists flask a feedback message
+        # Step 3 If not, create a new user
+        # Step 4 Add user to database
+        # Step 5 if admin is true, redirect to admin page, else send to index page
+        # Step 6 Flash message to feedback success
+    return render_template("register.html")
+
 
 
 @app.route("/recipes")
@@ -23,18 +40,6 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route("/admin")
-def admin():
-    return render_template("admin.html")
-
-
-@app.route("/register", methods =['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        # Links to form to get register info
-    return render_template("register.html")
-
-
 @app.route("/account")
 def account():
     return render_template("account.html")
@@ -43,6 +48,12 @@ def account():
 @app.route("/index")
 def index():
     return render_template("index.html")
+    
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
 
 
 @app.route("/add_a_recipe", methods =['GET', 'POST'])
