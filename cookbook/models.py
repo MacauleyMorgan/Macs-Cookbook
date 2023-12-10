848ae6b5c92ae9b2d092c754a67a49ec9589840e
Macauley Morgan <macauley.main@gmail.com>
2023-12-10 08:35:08 +0000

    Added lname, email, pwd fields to db model

--- cookbook/models.py
+++ cookbook/models.py
@@ -4,6 +4,10 @@ class User(db.Model):
     # Schema for creating user in db
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(25), nullable=False)
+    last_name = db.Column(db.String(25), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(25), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False, nullable=False)
     recipes = db.relationship("recipe", backref="user", cascade="all, delete", lazy=True)
 
     def __repr__(self):
